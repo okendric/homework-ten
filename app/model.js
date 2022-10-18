a81ad2cccfc5978b72fc28c840e3633adddc2a7d
okendric <okendric@iu.edu>
2022-10-17 23:51:41 -0400

    Initial commit

---
+++ app/model.js
@@ -0,0 +1,577 @@
+    var _homeContent = `<div class="splash">
+    <div class="overlay"></div>
+    <img src="images/hero.jpg" alt="" />
+  </div>
+
+  <div class="home">
+    <section class="hero">
+      <h1>travel-fly</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <br />Fugiat aliquid minus nemo sed est.
+      </p>
+      <div id="aboutButton" onclick="toAbout()" class="read-more">READ MORE</div>
+    </section>
+  </div>
+</div>
+
+<section class="promo-trips">
+  <div class="container">
+    <div class="tripBox">
+      <img src="images/tour/tour-israel/israel-tour-01.jpg" alt="" />
+      <div class="elements">
+        <h1>ISREAL</h1>
+        <h3>FROM $1000</h3>
+        <div id="isreal" class="learnMore">LEARN MORE</div>
+      </div>
+    </div>
+
+    <div class="tripBox">
+      <img src="images/tour/tour-usa/usa-tour-03.jpg" alt="" />
+      <div class="elements">
+        <h1>U.S.A</h1>
+        <h3>FROM $1300</h3>
+        <div id="toUSAButton" onclick="toUSA()" class="learnMore">LEARN MORE</div>
+      </div>
+    </div>
+
+    <div class="tripBox">
+      <img src="images/tour/tour-aus/aus-tour-03.jpg" alt="" />
+      <div class="elements">
+        <h1>AUSTRALIA</h1>
+        <h3>FROM $1700</h3>
+        <div id="toAUSButton" onclick="toAustralia()" class="learnMore">LEARN MORE</div>
+      </div>
+    </div>
+    </div>
+  </div>
+</section>
+
+<section class="booking">
+  <div class="booking-wrapper">
+    <p id="Booking">BOOKING FORM</p>
+    <input id="Name" type="text" placeholder="    name..." >
+
+    <input id="Email" type="email" placeholder="    email..." >
+
+    <input id="Country" type="text" placeholder="    country..." >
+
+    <input id="Hotel" type="text" placeholder="    hotel..." >
+
+    <p id="CheckInLabel">Check-in</p>
+    <input id="CheckIn" type="date">
+
+    <p id="CheckOutLabel">Check-out</p>
+    <input id="CheckOut" type="date">
+    
+    <p id="Adult">Adult</p>
+    <select name="" id="AdultDropbox">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+    </select>
+
+    <p id="Children">Children</p>
+    <select name="" id="ChildDropbox">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+    </select>
+
+    <p id="Rooms">Rooms</p>
+    <select name="" id="RoomDropbox">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+    </select>
+
+    <input id="Textbox" type="text" placeholder="  message...">
+  </div>
+
+<input class="submit" type="submit" value="SUBMIT">
+
+
+
+</section>`;
+    var _aboutContent = `  
+    <div class="about">
+      <div class="title">
+        <h1>ABOUT:</h1>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+        ab illo inventore veritatis et quasi architecto beatae vitae dicta
+        sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+        qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+        dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+        quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+        aliquam quaerat voluptatem.
+      </p>
+    </div>
+
+    <div class="client-quotes">
+      <div class="client-title">
+        <h1>CLIENT QUOTES:</h1>
+      </div>
+    </div>
+
+    <div class="wrapperAbout">
+      <div class="person-1">
+        <div class="image-1">
+          <img src="images/about-client-01.jpg" alt="" />
+        </div>
+
+        <div class="text-1">
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </p>
+        </div>
+
+        <div class="author">
+          <p>- MIRANDA BROWN</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="wrapperAbout">
+      <div class="person-1">
+        <div class="image-1">
+          <img src="images/about-client-02.jpg" alt="" />
+        </div>
+
+        <div class="text-1">
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </p>
+        </div>
+
+        <div class="author">
+          <p>- JOHNATHAN WES</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="spacer"></div>`;
+    var _toursContent = `    
+    <!----HEADER IMAGE------------------------->
+    <div class="headerTourList">
+      <div class="text"><h1>TOURS:</h1></div>
+
+      <img src="images/tour/tours-header.jpg" alt="" />
+    </div>
+
+    <!--------TOUR IMAGES-------------------------->
+    <div class="wrapperTourList">
+      <div class="tour">
+        <img src="images/tour/tour-01.jpg" alt="" />
+        <div class="ontop">
+          <h1>ISREAL</h1>
+          <div class="price">From: $1000</div>
+          <div id="toIsrealButton" class="learnMore" onclick="toIsreal()">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-02.jpg" alt="" />
+        <div class="ontop">
+          <h1>U.S.A</h1>
+          <div class="price">From: $1500</div>
+          <div id="toUSAButton" onclick="toUSA()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-03.jpg" alt="" />
+        <div class="ontop">
+          <h1>AUSTRALIA</h1>
+          <div class="price">From: $1800</div>
+          <div id="toAUSButton" onclick="toAustralia()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-04.jpg" alt="" />
+        <div class="ontop">
+          <h1 style="font-size: 28px; margin-bottom: 8px">NEW ZEALAND</h1>
+          <div class="price">From: $1200</div>
+          <div id="NZButton" onclick="toNZ()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-05.jpg" alt="" />
+        <div class="ontop">
+          <h1>FRANCE</h1>
+          <div class="price">From: $2500</div>
+          <div id="FranceButton" onclick="toFrance()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-06.jpg" alt="" />
+        <div class="ontop">
+          <h1>EGYPT</h1>
+          <div class="price">From: $900</div>
+          <div id="egyptButton" onclick="toEgypt()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-07.jpg" alt="" />
+        <div class="ontop">
+          <h1>JAPAN</h1>
+          <div class="price">From: $1300</div>
+          <div id="japanButton" onclick="toJapan()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-08.jpg" alt="" />
+        <div class="ontop">
+          <h1>CANADA</h1>
+          <div class="price">From: $2000</div>
+          <div id="canadaButton" onclick="toCanada()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="tour">
+        <img src="images/tour/tour-09.jpg" alt="" />
+        <div class="ontop">
+          <h1>U.A.E</h1>
+          <div class="price">From: $3000</div>
+          <div id="UAEButton" onclick="toUAE()" class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    var _specialOffersContent = `
+ <!-------------CONTENT AREA--------------->
+    <section class="special">
+      <div class="specialWrapper">
+        <div class="titleSpecial">
+          <h1>special offer(s):</h1>
+        </div>
+
+        <div class="specialImage">
+          <img src="images/special-offer-01.jpg" alt="" />
+        </div>
+
+        <div class="specialBox">
+          <div class="titleSpecial-2">
+            <h1>barcelona, spain</h1>
+          </div>
+
+          <div class="discount">
+            <p>(20% off!)</p>
+          </div>
+
+          <div class="specialParagraph">
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo.
+            </p>
+          </div>
+
+          <div class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="specialWrapper">
+        <div class="specialImage">
+          <img src="images/special-offer-02.jpg" alt="" />
+        </div>
+
+        <div class="specialBox">
+          <div class="titleSpecial-2">
+            <h1>bangkok, thailand</h1>
+          </div>
+
+          <div class="discount">
+            <p>(10% off!)</p>
+          </div>
+
+          <div class="specialParagraph">
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo.
+            </p>
+          </div>
+
+          <div class="learnMore">
+            <p>Learn More</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="specialSpace"></div>
+`;
+    var _blogContent = `
+    <!------MAIN CONTENT----------------------->
+    <div class="blogContainer">
+      <section class="blogGroup">
+        <div class="blogBox">
+          <h1>blog:</h1>
+        </div>
+
+        <div class="blogDate">
+          <h1>06</h1>
+          <h2>jun</h2>
+        </div>
+
+        <section class="blogGroup-2">
+          <div class="blogImage">
+            <img src="images/blog.jpg" alt="" />
+          </div>
+
+          <div class="blogParagraph">
+            <h4>Sed et persipiatis unde omnis iste natus</h4>
+            <h5>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo.
+            </h5>
+          </div>
+        </section>
+      </section>
+
+      <section class="blogGroup">
+        <div class="blogBox">
+          <h1>blog:</h1>
+        </div>
+
+        <div class="blogDate">
+          <h1>23</h1>
+          <h2>jul</h2>
+        </div>
+
+        <section class="blogGroup-2">
+          <div class="blogImage">
+            <img src="images/blog.jpg" alt="" />
+          </div>
+
+          <div class="blogParagraph">
+            <h4>Sed et persipiatis unde omnis iste natus</h4>
+            <h5>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo.
+            </h5>
+          </div>
+        </section>
+      </section>
+    </div>`;
+    var _contactContent = `
+     <div class="contactWrapper">
+      <div class="contactBox">
+        <h1>Contact us:</h1>
+      </div>
+      <div class="contact">
+        <div class="map">
+          <div class="map-image">
+            <img src="images/map.png" alt="" />
+          </div>
+          <div class="map-address">
+            <p>
+              travel-fly Inc. <br />8901 Marmora Road,<br />
+              Glasgow, D04 89GR.<br />
+              Freephone:+1 800 559 6580<br />
+              Telephone:+1 800 603 6035<br />
+              FAX:+1 800 889 9898<br />
+              E-mail: mail@travelfly.org
+            </p>
+          </div>
+        </div>
+        <div class="contactForm">
+          <input
+            class="textFields"
+            type="text"
+            name="yourName"
+            id="yourName"
+            placeholder="Your name..."
+          />
+          <input
+            class="textFields"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address..."
+          />
+          <input
+            class="textFields"
+            type="text"
+            name="company"
+            id="company"
+            placeholder="Company..."
+          />
+
+          <input
+            class="longText"
+            type="text"
+            name="longText"
+            id="longText"
+            placeholder="Message..."
+          />
+
+          <div class="contactSubmit">
+            <p>send message</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+  <div class="contactSpace"></div>`;
+
+  var _isrealContent = `<!-------THIS IS THE HEADER------------------->
+  <div class="headerTourPages">
+    <h1>Isreal tour package:</h1>
+    <img src="images/tour/tour-01.jpg" alt="" />
+    <p>Starting from $1000 (prices may vary)</p>
+  </div>
+
+  <!-----THREE IMAGES--------------------------->
+  <div class="threeImages">
+    <img src="images/tour/tour-israel/israel-tour-01.jpg" alt="" />
+
+    <img src="images/tour/tour-israel/israel-tour-02.jpg" alt="" />
+
+    <img src="images/tour/tour-israel/israel-tour-03.jpg" alt="" />
+  </div>
+
+  <!--------DAY NUMBERS + PARAGRAPHS----------->
+  <div class="wrapperTourPages">
+    <div class="paragraph">
+      <div class="title">
+        <p>Day #1:</p>
+      </div>
+      <div class="text">
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+          eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+          sed quia non numquam eius modi tempora incidunt ut labore et dolore
+          magnam aliquam quaerat voluptatem.
+        </p>
+      </div>
+    </div>
+
+    <div class="paragraph">
+      <div class="title">
+        <p>Day #2:</p>
+      </div>
+      <div class="text">
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+          eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+          sed quia non numquam eius modi tempora incidunt ut labore et dolore
+          magnam aliquam quaerat voluptatem.
+        </p>
+      </div>
+    </div>
+
+    <div class="paragraph">
+      <div class="title">
+        <p>Day #3:</p>
+      </div>
+      <div class="text">
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+          eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+          sed quia non numquam eius modi tempora incidunt ut labore et dolore
+          magnam aliquam quaerat voluptatem.
+        </p>
+      </div>
+    </div>
+
+    <div class="bookNow">
+      <p>Book now</p>
+    </div>
+  </div>`
+
+  function initListenersR() {
+    $("div .learnMore").click(function(e) {
+        // this stores the id from the anchor tag
+        let aID = e.currentTarget.id;
+        let contentID = aID + "Content";
+        console.log("This should say something.");
+
+        MODEL.updateView(contentID)
+    });
+}
+
+    export function updateView(pageName){
+    // console.log("Model:" + pageName);
+    var pageContent = "_" + pageName;
+    $("#app").html(eval(pageContent));
+
+    if(pageName == "homeContent") {
+      $("#nav-holder").removeClass("darkNav");
+    } else {
+      $("#nav-holder").addClass("darkNav");
+    }
+
+}
